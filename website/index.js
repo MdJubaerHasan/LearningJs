67f5f7f69ae86352ce602e5ef792fdab1742005f
Octane <jubaerhasan1147@gmail.com>
2025-04-28 11:55:33 +0100

    learned DOM

--- website/index.js
+++ website/index.js
@@ -2515,3 +2515,24 @@ try {
 
 console.log("You have reached the end"); */
 
+
+// ========================>>> DOM
+//// DOM = Document Object Model
+////       Object{} that represents the page you see in the web
+////       browser and provides you with an API to interact with it.
+////       Web browser constructs the DOM when it load an HTML document.
+////       and structures all the elements in a tree-like representation.
+////       JavaScript can access the DOM to dynamically
+////       change the content, structure and style of a web page.
+
+document.title = "You are in control";
+
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)";
+
+console.log(document);
+console.dir(document);
+
+const username = "Bro code";
+
+const welcomeMsg = document.getElementById("welcome-msg");
+welcomeMsg.textContent += username === "" ? "Guest" : " " + username;
