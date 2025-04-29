89e2973ef5b77788ea506b7fcfecac23b4263ed5
Octane <jubaerhasan1147@gmail.com>
2025-04-29 13:41:53 +0100

    learned how to show/hide html elements

--- website/index.js
+++ website/index.js
@@ -2934,3 +2934,35 @@ document.addEventListener("keydown", event =>{
         myBox.style.left = `${x}px`;
     }
 }); */
+
+
+
+// ========================>>> Show and Hide Elements
+
+const myButton = document.getElementById("myBtn");
+const myImage = document.getElementById("myImg");
+
+// myButton.addEventListener("click", event=>{
+
+//     if(myImage.style.display === "none"){
+//         myImage.style.display = "block";
+//         myButton.textContent = "Hide";
+//     }else{
+//         myImage.style.display = "none";
+//         myButton.textContent = "Show";
+//     }
+    
+// });
+
+
+myButton.addEventListener("click", event=>{
+
+    if(myImage.style.visibility === "hidden"){
+        myImage.style.visibility = "visible";
+        myButton.textContent = "Hide";
+    }else{
+        myImage.style.visibility = "hidden";
+        myButton.textContent = "Show";
+    }
+    
+});
